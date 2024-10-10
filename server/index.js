3be554b7865df7c22b63d28c7622c26fe3acbef0
Mattazz <matthewazada101@gmail.com>
2024-10-10 11:36:50 -0400

    start of backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // Load environment variables from .env file
 dotenv.config();
@@ -9,10 +13,31 @@ const app = express();
 // eslint-disable-next-line no-undef
 const port = process.env.PORT || 3000;
 
+// Get directory name of the current module file
+const __filename = fileURLToPath(import.meta.url);
+console.log(`__filename = ${__filename}`);
+
+
+const __dirname = dirname(__filename)
+console.log(`__dirname = ${__dirname}`);
+
+
+// Serve static files from the react app
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+// API endpoint
+app.get('/api/hello', (req, res) => {
+    res.json({message: "Hello from the server!"})
+})
+
+// Serve the React app for any other route
+app.get('*', (req, res) => {
+    const filePath = path.join(__dirname, '..', 'dist', 'index.html');
+    console.log(`filePath = ${filePath}`);
+    
+    res.sendFile(filePath);
 });
 
 app.listen(port, () => {
