42acfa15c34ca853953111927b3787ce1634b6bb
Mattazz <matthewazada101@gmail.com>
2024-10-11 08:23:49 -0400

    minor styling and validation fixes

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -6,9 +6,10 @@ function Signup() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const [validationMessage, setValidationMessage] = useState('');
 
-  const onSubmit = (data) => {
-    console.log('Form Data:', data); // Log form data to the console
 
+  const onSubmit = (data) => {
+    console.log('Form Data:', data); 
+    
     fetch('/signup', {
       method: 'POST',
       headers: {
@@ -33,7 +34,7 @@ function Signup() {
       <h1 className='center'>Sign up</h1>
 
       <div className="flex" id="signup-container">
-        <form className="form-flex" onSubmit={handleSubmit(onSubmit)}>
+        <form className="form-flex-col" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="first-name">First Name:</label>
           <input
             type="text"
@@ -41,7 +42,7 @@ function Signup() {
             {...register('first-name', { required: 'First name is required' })}
             placeholder="First name"
           />
-          {errors['first-name'] && <div>{errors['first-name'].message}</div>}
+          {errors['first-name'] && <div className='error-message'>{errors['first-name'].message}</div>}
 
           <label htmlFor="last-name">Last Name:</label>
           <input
@@ -50,7 +51,7 @@ function Signup() {
             {...register('last-name', { required: 'Last name is required' })}
             placeholder="Last name"
           />
-          {errors['last-name'] && <div>{errors['last-name'].message}</div>}
+          {errors['last-name'] && <div className='error-message'>{errors['last-name'].message}</div>}
 
           <label htmlFor="username">Username:</label>
           <input
@@ -59,7 +60,7 @@ function Signup() {
             {...register('username', { required: 'Username is required', minLength: { value: 4, message: 'Username must be at least 4 characters' } })}
             placeholder="username"
           />
-          {errors.username && <div>{errors.username.message}</div>}
+          {errors.username && <div className='error-message'>{errors.username.message}</div>}
 
           <label htmlFor="password">Password:</label>
           <input
@@ -68,7 +69,7 @@ function Signup() {
             {...register('password', { required: 'Password is required', minLength: { value: 6, message: 'Password must be at least 6 characters' } })}
             placeholder="password"
           />
-          {errors.password && <div>{errors.password.message}</div>}
+          {errors.password && <div className='error-message'>{errors.password.message}</div>}
 
           <label htmlFor="confirm-password">Confirm Password:</label>
           <input
@@ -80,12 +81,12 @@ function Signup() {
             })}
             placeholder="Confirm password"
           />
-          {errors['confirm-password'] && <div>{errors['confirm-password'].message}</div>}
+          {errors['confirm-password'] && <div className='error-message'>{errors['confirm-password'].message}</div>}
 
           <button type="submit">Submit</button>
         </form>
       </div>
-        {validationMessage && <div className='center red'>{validationMessage}</div>}
+        {validationMessage && <div className='center error-message'>{validationMessage}</div>}
     </>
   );
 }
